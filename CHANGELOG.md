764039847d4be508bae34d88de1533d0b6fb621b
boot1110001 <ppuubblliicc@protonmail.com>
2019-09-20 12:35:04 +0200

    ### Added - Archívos básicos para iniciar el repositorio. - Archivo de instalación de paquetes básicos de los repositorios de debian 10 buster.

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+All notable changes to [gnu-linux-install-configs](https://github.com/boot1110001/gnu-linux-install-configs) will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## Release
+
+## 20190920a - 2019-09-20 11:27
+> by inoro <ppuubblliicc@protonmail.com>
+### Added
+- Archívos básicos para iniciar el repositorio.
+- Archivo de instalación de paquetes básicos de los repositorios de debian 10 buster.
---
+++ debian 10 buster/packages-installation/install-from-repositories.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# (c) inoro [20190920a]
+# [install-from-repositories]
+
+packagelist_install=(
+    # utilidades básicas (no gráficas)
+    build-essential
+    linux-headers-$(uname -r)
+    nano
+    net-tools
+    tree
+    tldr
+    htop
+    nmap
+    neofetch
+    git
+    openjdk-11-jdk
+
+    # utilidades básicas (gráficas)
+    simple-scan
+    brasero
+    gparted
+    easytag
+    #pyrenamer
+    filezilla
+    wireshark
+
+    # editores de texto
+    gedit
+    geany
+    libreoffice
+
+    # dependencias
+    ### gnome shell system monitor extension dependences
+    gir1.2-gtop-2.0
+    gir1.2-nm-1.0
+    gir1.2-clutter-1.0
+
+    # reproductores de vídeo y audio
+    vlc
+    rhythmbox
+
+    # cliente bittorrent
+    qbittorrent
+
+    # pdf
+    evince
+    okular
+    xournal
+
+    # procesamiento de imágenes
+    gimp
+    inkscape
+
+    # procesamiento de audio
+    audacity
+
+    # editor de diagramas
+    dia
+
+    # editor 2D
+    librecad
+
+    # games
+    gnome-mahjongg
+
+    # chromium
+    chromium
+
+    # astronomía
+    stellarium
+
+    # latex
+    texlive-full
+    texmaker
+)
+
+packagelist_remove=(
+    transmission-gtk
+    shotwell
+)
+
+apt update
+apt upgrade -y
+apt install ${packagelist_install[@]} -y
+apt remove ${packagelist_remove[@]}
+apt autoremove
