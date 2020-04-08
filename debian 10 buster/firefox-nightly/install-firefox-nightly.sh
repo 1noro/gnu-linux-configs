67174301938f15985dc4376fd7df9b5404b30169
boot1110001 <ppuubblliicc@protonmail.com>
2020-04-08 21:47:15 +0200

    agregado un actualizador grafico

--- debian 10 buster/firefox-nightly/install-firefox-nightly.sh
+++ debian 10 buster/firefox-nightly/install-firefox-nightly.sh
@@ -6,9 +6,21 @@
 #~ LANG="en-US"
 LANG="es-ES"
 
+FILE=./FirefoxNightlySetup.tar.bz2
+if [ -f "$FILE" ]; then
+    rm "$FILE"
+fi
+
 wget -O FirefoxNightlySetup.tar.bz2 "http://download.mozilla.org/?product=firefox-nightly-latest&os=linux64&lang=$LANG"
+echo ""
+echo "Extracting..."
 tar xjf FirefoxNightlySetup.tar.bz2 -C ./
-rm -r /opt/firefox-nightly ||: # == || true (en caso de fallar no muestra error
+
+DIR=/opt/firefox-nightly
+if [ -d "$DIR" ]; then
+    rm -r "$DIR"
+fi
+
 mkdir -p /opt/firefox-nightly
 mv ./firefox/* /opt/firefox-nightly
 
@@ -23,11 +35,24 @@ echo "Categories=Network;WebBrowser;" >> /usr/share/applications/firefox-nightly
 echo "MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/vnd.mozilla.xul+xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;" >> /usr/share/applications/firefox-nightly.desktop
 echo "StartupNotify=true" >> /usr/share/applications/firefox-nightly.desktop
 
-rm /usr/local/bin/firefox-nightly ||: # == || true (en caso de fallar no muestra error
+FILE=/usr/local/bin/firefox-nightly
+if [ -f "$FILE" ]; then
+    rm "$FILE"
+fi
 ln -s /opt/firefox-nightly/firefox /usr/local/bin/firefox-nightly
 
 cp /opt/firefox-nightly/browser/chrome/icons/default/default128.png /usr/share/icons/firefox-nightly.png
 cp /opt/firefox-nightly/browser/chrome/icons/default/default128.png "/usr/share/icons/Firefox Nightly.png"
 
+# Updater
+cp ./update-firefox-nightly.sh /opt/update-firefox-nightly.sh
+chmod +x /opt/update-firefox-nightly.sh
+
+cp ./update-firefox-nightly.desktop /usr/share/applications/update-firefox-nightly.desktop
+
+cp ./icons/update_128.png /usr/share/icons/update-firefox-nightly.png
+cp ./icons/update_128.png "/usr/share/icons/Update Firefox Nightly.png"
+
+# Clean
 rm FirefoxNightlySetup.tar.bz2
 rm -r ./firefox
--- debian 10 buster/firefox-nightly/update-firefox-nightly.sh
+++ debian 10 buster/firefox-nightly/update-firefox-nightly.sh
@@ -6,11 +6,28 @@
 #~ LANG="en-US"
 LANG="es-ES"
 
+FILE=./FirefoxNightlySetup.tar.bz2
+if [ -f "$FILE" ]; then
+    rm "$FILE"
+fi
+
 wget -O FirefoxNightlySetup.tar.bz2 "http://download.mozilla.org/?product=firefox-nightly-latest&os=linux64&lang=$LANG"
+echo ""
+echo "Extracting..."
 tar xjf FirefoxNightlySetup.tar.bz2 -C ./
-rm -r /opt/firefox-nightly
+
+DIR=/opt/firefox-nightly
+if [ -d "$DIR" ]; then
+    rm -r "$DIR"
+fi
+
 mkdir -p /opt/firefox-nightly
 mv ./firefox/* /opt/firefox-nightly
 
+# Claeaning
 rm FirefoxNightlySetup.tar.bz2
 rm -r ./firefox
+
+echo ""
+echo "Press any key..."
+read
