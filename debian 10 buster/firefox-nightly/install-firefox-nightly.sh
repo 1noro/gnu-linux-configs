028f9908e9888bbcae13714c2c36cf211b21882a
boot1110001 <ppuubblliicc@protonmail.com>
2020-04-09 20:34:14 +0200

    pasado todo por variables

--- debian 10 buster/firefox-nightly/install-firefox-nightly.sh
+++ debian 10 buster/firefox-nightly/install-firefox-nightly.sh
@@ -5,63 +5,85 @@
 
 #~ LANG="en-US"
 LANG="es-ES"
-URL="http://download.mozilla.org/?product=firefox-nightly-latest&os=linux64&lang=$LANG"
-DOWNLOADED_FILE="FirefoxNightlySetup.tar.bz2"
 EXTRACTED_DIR="firefox"
-DESTINATION_DIR="/opt/firefox-nightly"
-DESKTOP_FILE="firefox-nightly.desktop"
+
+URL="http://download.mozilla.org/?product=firefox-nightly-latest&os=linux64&lang=$LANG"
 NAME="Firefox Nightly"
 SHELL_NAME="firefox-nightly"
 
-echo ">> Cleaning the installation stage..."
-if [ -f "$DOWNLOADED_FILE" ]; then
-    rm "$DOWNLOADED_FILE"
-fi
+DOWNLOADED_FILE="$SHELL_NAME-setup.tar.bz2"
+INSTALLATION_DIR="/opt/$SHELL_NAME"
+DESKTOP_FILE="$SHELL_NAME.desktop"
 
+# --- CLEANING PREVIOUS INSTALLATIONS ------------------------------------------
+echo ">> Cleaning the installation stage..."
+# Remove EXTRACTED_DIR if exists
 if [ -d "$EXTRACTED_DIR" ]; then
     rm -r "$EXTRACTED_DIR"
 fi
 
-if [ -d "$DESTINATION_DIR" ]; then
-    rm -r "$DESTINATION_DIR"
+# Remove DOWNLOADED_FILE if exists
+if [ -f "$DOWNLOADED_FILE" ]; then
+    rm "$DOWNLOADED_FILE"
 fi
 
+# Remove shell link if exists
 if [ -f "/usr/local/bin/$SHELL_NAME" ]; then
     rm "/usr/local/bin/$SHELL_NAME"
 fi
 
-echo ">> Downloading..."
-wget wget -q --show-progress -O "$DOWNLOADED_FILE" "$URL"
-echo ">> Extracting..."
-tar xjf ./"$DOWNLOADED_FILE" -C ./
+# Remove gnome shell link if exists
+if [ -f "/usr/share/applications/$DESKTOP_FILE" ]; then
+    rm "/usr/share/applications/$DESKTOP_FILE"
+fi
 
-mkdir -p "$DESTINATION_DIR"
-echo ">> Moving..."
-mv ./"$EXTRACTED_DIR"/* "$DESTINATION_DIR"
+# Remove icons if exists
+if [ -f "/usr/share/icons/$NAME.png" ]; then
+    rm "/usr/share/icons/$NAME.png"
+fi
+
+if [ -f "/usr/share/icons/$SHELL_NAME.png" ]; then
+    rm "/usr/share/icons/$SHELL_NAME.png"
+fi
+
+# Remove INSTALLATION_DIR link if exists
+if [ -d "$INSTALLATION_DIR" ]; then
+    rm -r "$INSTALLATION_DIR"
+fi
+
+# --- INSTALLATION -------------------------------------------------------------
+echo ">> Downloading $DOWNLOADED_FILE"
+wget wget -q --show-progress -O "$DOWNLOADED_FILE" "$URL"
+echo ">> Extracting $DOWNLOADED_FILE"
+tar xjf "$DOWNLOADED_FILE" -C ./
+
+mkdir -p "$INSTALLATION_DIR"
+echo ">> Copying $EXTRACTED_DIR/* --> $INSTALLATION_DIR"
+cp -r "./$EXTRACTED_DIR/"* "$INSTALLATION_DIR"
 
 echo ">> Copying icons..."
-cp "$DESTINATION_DIR" /browser/chrome/icons/default/default128.png /usr/share/icons/"$SHELL_NAME".png
-cp "$DESTINATION_DIR" /browser/chrome/icons/default/default128.png "/usr/share/icons/$NAME.png"
+cp "$INSTALLATION_DIR/browser/chrome/icons/default/default128.png" "/usr/share/icons/$SHELL_NAME.png"
+cp "$INSTALLATION_DIR/browser/chrome/icons/default/default128.png" "/usr/share/icons/$NAME.png"
 
 echo ">> Copying gnome shell link..."
-cp ./"$DESKTOP_FILE" /usr/share/applications/"$DESKTOP_FILE"
+cp "./$DESKTOP_FILE" "/usr/share/applications/$DESKTOP_FILE"
 
 echo ">> Creating shell link..."
-ln -s "$DESTINATION_DIR"/firefox /usr/local/bin/"$SHELL_NAME"
+ln -s "$INSTALLATION_DIR/firefox" "/usr/local/bin/$SHELL_NAME"
 
-echo ">> Cleaning 'FirefoxNightlySetup.tar.bz2' file"
+echo ">> Deleting $DOWNLOADED_FILE"
 rm "$DOWNLOADED_FILE"
-echo ">> Cleaning 'firefox' folder"
+echo ">> Deleting $EXTRACTED_DIR folder"
 rm -r "$EXTRACTED_DIR"
 
-# Updater
+# --- INSTALLING THE UPDATER ---------------------------------------------------
 echo ">> Copying updater script..."
-cp ./update-firefox-nightly.sh /opt/update-firefox-nightly.sh
-chmod +x /opt/update-firefox-nightly.sh
+cp "./update-$SHELL_NAME.sh" "/opt/update-$SHELL_NAME.sh"
+chmod +x "/opt/update-$SHELL_NAME.sh"
 
 echo ">> Copying updater icons..."
-cp ./icons/update_128.png /usr/share/icons/update-firefox-nightly.png
-cp ./icons/update_128.png "/usr/share/icons/Update Firefox Nightly.png"
+cp ./icons/update_128.png "/usr/share/icons/update-$SHELL_NAME.png"
+cp ./icons/update_128.png "/usr/share/icons/Update $NAME.png"
 
 echo ">> Copying updater script gnome shell link..."
-cp ./update-firefox-nightly.desktop /usr/share/applications/update-firefox-nightly.desktop
+cp "./update-$SHELL_NAME.desktop" "/usr/share/applications/update-$SHELL_NAME.desktop"
