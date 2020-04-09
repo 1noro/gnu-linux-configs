a983e7a54631bd555b716a8607fbc5545ded13fe
boot1110001 <ppuubblliicc@protonmail.com>
2020-04-09 17:28:52 +0200

    a medias con la universalización del script para pasarlo todo por variables

--- debian 10 buster/firefox-nightly/install-firefox-nightly.sh
+++ debian 10 buster/firefox-nightly/install-firefox-nightly.sh
@@ -5,44 +5,54 @@
 
 #~ LANG="en-US"
 LANG="es-ES"
+URL="http://download.mozilla.org/?product=firefox-nightly-latest&os=linux64&lang=$LANG"
+DOWNLOADED_FILE="FirefoxNightlySetup.tar.bz2"
+EXTRACTED_DIR="firefox"
+DESTINATION_DIR="/opt/firefox-nightly"
+DESKTOP_FILE="firefox-nightly.desktop"
+NAME="Firefox Nightly"
+SHELL_NAME="firefox-nightly"
 
-FILE=./FirefoxNightlySetup.tar.bz2
-if [ -f "$FILE" ]; then
-    rm "$FILE"
+echo ">> Cleaning the installation stage..."
+if [ -f "$DOWNLOADED_FILE" ]; then
+    rm "$DOWNLOADED_FILE"
+fi
+
+if [ -d "$EXTRACTED_DIR" ]; then
+    rm -r "$EXTRACTED_DIR"
+fi
+
+if [ -d "$DESTINATION_DIR" ]; then
+    rm -r "$DESTINATION_DIR"
+fi
+
+if [ -f "/usr/local/bin/$SHELL_NAME" ]; then
+    rm "/usr/local/bin/$SHELL_NAME"
 fi
 
 echo ">> Downloading..."
-wget -O FirefoxNightlySetup.tar.bz2 "http://download.mozilla.org/?product=firefox-nightly-latest&os=linux64&lang=$LANG"
+wget wget -q --show-progress -O "$DOWNLOADED_FILE" "$URL"
 echo ">> Extracting..."
-tar xjf FirefoxNightlySetup.tar.bz2 -C ./
+tar xjf ./"$DOWNLOADED_FILE" -C ./
 
-DIR=/opt/firefox-nightly
-if [ -d "$DIR" ]; then
-    rm -r "$DIR"
-fi
-
-mkdir -p /opt/firefox-nightly
+mkdir -p "$DESTINATION_DIR"
 echo ">> Moving..."
-mv ./firefox/* /opt/firefox-nightly
+mv ./"$EXTRACTED_DIR"/* "$DESTINATION_DIR"
 
 echo ">> Copying icons..."
-cp /opt/firefox-nightly/browser/chrome/icons/default/default128.png /usr/share/icons/firefox-nightly.png
-cp /opt/firefox-nightly/browser/chrome/icons/default/default128.png "/usr/share/icons/Firefox Nightly.png"
+cp "$DESTINATION_DIR" /browser/chrome/icons/default/default128.png /usr/share/icons/"$SHELL_NAME".png
+cp "$DESTINATION_DIR" /browser/chrome/icons/default/default128.png "/usr/share/icons/$NAME.png"
 
 echo ">> Copying gnome shell link..."
-cp ./firefox-nightly.desktop /usr/share/applications/firefox-nightly.desktop
+cp ./"$DESKTOP_FILE" /usr/share/applications/"$DESKTOP_FILE"
 
 echo ">> Creating shell link..."
-FILE=/usr/local/bin/firefox-nightly
-if [ -f "$FILE" ]; then
-    rm "$FILE"
-fi
-ln -s /opt/firefox-nightly/firefox /usr/local/bin/firefox-nightly
+ln -s "$DESTINATION_DIR"/firefox /usr/local/bin/"$SHELL_NAME"
 
 echo ">> Cleaning 'FirefoxNightlySetup.tar.bz2' file"
-rm ./FirefoxNightlySetup.tar.bz2
+rm "$DOWNLOADED_FILE"
 echo ">> Cleaning 'firefox' folder"
-rm -r ./firefox
+rm -r "$EXTRACTED_DIR"
 
 # Updater
 echo ">> Copying updater script..."
