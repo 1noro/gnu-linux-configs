0e7cc3fbd6c7dcaede255a1c46f740659ca98227
boot1110001 <ppuubblliicc@protonmail.com>
2020-04-24 22:40:22 +0200

    instalación de mull vad desde aur

--- archlinux/install-packages-extra-aur.sh
+++ archlinux/install-packages-extra-aur.sh
@@ -53,8 +53,28 @@ cd ..
 
 ## VPNs
 # mullvad
-# ejemplo de agregar clave a gnupg: gpg --recv-keys 8F0871F202119294
+# hay que mirar el PKGBUILD y comprobar que claves se necesitan
+# PKGBUILD: https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=mullvad-vpn
+# dónde encontrar las claves: https://mullvad.net/en/help/open-source/
+# gía de mullvad para confiar en las claves: https://mullvad.net/en/help/verifying-signatures/
+# claves necesarias a dia 202004242239
+# EA0A77BF9E115615FC3BD8BC7653B940E494FE87 Linus Färnstrand (code signing key) <linus@mullvad.net>
+# 8339C7D2942EB854E3F27CE5AEE9DECFD582E984 David Lönnhager (code signing) <david.l@mullvad.net>
+# las descargo a mi pc:
+gpg2 --keyserver pool.sks-keyservers.net --recv-keys EA0A77BF9E115615FC3BD8BC7653B940E494FE87
+gpg2 --keyserver pool.sks-keyservers.net --recv-keys 8339C7D2942EB854E3F27CE5AEE9DECFD582E984
+# las edito:
+gpg2 --edit-key EA0A77BF9E115615FC3BD8BC7653B940E494FE87
+# > trust
+# > 5
+# > s
+gpg2 --edit-key 8339C7D2942EB854E3F27CE5AEE9DECFD582E984
+# > trust
+# > 5
+# > s
+# ya hora puedo instalar el paquete
 git clone https://aur.archlinux.org/mullvad-vpn.git
 cd mullvad-vpn
 makepkg -sri
+systemctl enable mullvad-daemon
 cd ..
