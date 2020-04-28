55a15d1d0f5e407e370c93ab4040ce78773ace78
boot1110001 <ppuubblliicc@protonmail.com>
2020-04-28 16:49:08 +0200

    das

--- archlinux/check-update-aur.sh
+++ archlinux/check-update-aur.sh
@@ -2,7 +2,7 @@
 for D in */; do
     if [[ $(git -C $D fetch) ]]; then
         git -C $D pull -q
-        echo -e "Package  \e[92m\e[1m$D\e[0m needs to be updated."
+        echo -e "Package \e[92m\e[1m$D\e[0m pulled. It needs to be updated."
     else
         echo -e "Package $D no needs to be updated."
     fi
