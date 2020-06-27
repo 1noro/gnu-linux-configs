e79e6d226dba061ee25aba692ce55388aab94372
1noro <ppuubblliicc@protonmail.com>
2020-06-28 00:23:49 +0200

    kf

--- raspios/install-packages-basic.sh
+++ raspios/install-packages-basic.sh
@@ -6,3 +6,6 @@
 apt update; \
 apt upgrade -y; \
 apt install nano net-tools dnsutils tree tldr htop nmap neofetch git curl iperf3 -y
+
+## RUN ONLY AS ROOT
+# echo "pi ALL=(ALL) PASSWD: ALL" > /etc/sudoers.d/010_pi-nopasswd
