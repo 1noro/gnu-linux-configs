e4a4c102e65e7cfe2119a5076f064738e1e1737c
1noro <ppuubblliicc@protonmail.com>
2020-07-09 12:05:06 +0200

    sdfg

---
+++ debian-10-buster/install-packages-basic-vm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## by inoro
+## RUN AS ROOT
+
+## basic install
+apt update; \
+apt upgrade -y; \
+apt install nano net-tools dnsutils tree tldr htop nmap neofetch git curl iperf3 -y
+
+## RUN ONLY AS ROOT
+# echo "pi ALL=(ALL) PASSWD: ALL" > /etc/sudoers.d/010_pi-nopasswd
